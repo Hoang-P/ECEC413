f30dc2ddf71885d5f2a3867f04529a11ea3860f6
Hoang-P <hpham315@gmail.com>
2020-05-06 19:49:09 -0400

    Changed to optimized version

--- assign2/jacobi/compute_gold.c
+++ assign2/jacobi/compute_gold.c
@@ -23,14 +23,22 @@ void compute_gold(const matrix_t A, matrix_t x, const matrix_t B, int max_iter)
     int num_iter = 0;
     
     while (!done) {
-        for (i = 0; i < num_rows; i++) {
-            double sum = 0.0;
-            for (j = 0; j < num_cols; j++) {
-                if (i != j)
-                    sum += A.elements[i * num_cols + j] * x.elements[j];
-            }
+        // for (i = 0; i < num_rows; i++) {
+        //     double sum = 0.0;
+        //     for (j = 0; j < num_cols; j++) {
+        //         if (i != j)
+        //             sum += A.elements[i * num_cols + j] * x.elements[j];
+        //     }
            
-            /* Update values for the unkowns for the current row. */
+        //     /* Update values for the unkowns for the current row. */
+        //     new_x.elements[i] = (B.elements[i] - sum)/A.elements[i * num_cols + i];
+        // }
+
+        for (i = 0; i < num_rows; i++) {
+            double sum = -A.elements[i * num_cols + i] * x.elements[i];
+            for (j = 0; j < num_cols; j++)
+                sum += A.elements[i * num_cols + j] * x.elements[j];
+            
             new_x.elements[i] = (B.elements[i] - sum)/A.elements[i * num_cols + i];
         }
 
