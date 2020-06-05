d0e7c8e0e08eaa9995b7c8170ebaac28bf449473
Hoang-P <hpham315@gmail.com>
2020-06-05 04:05:34 -0400

    Fixed small bug

--- assign5/conv/separable_convolution_kernel.cu
+++ assign5/conv/separable_convolution_kernel.cu
@@ -69,12 +69,38 @@ __global__ void convolve_columns_kernel_naive(float *result, float *input, float
 
 __global__ void convolve_rows_kernel_optimized(float *result, float *input, int num_cols, int num_rows, int half_width)
 {
+    // __shared__ float input_ts[THREAD_BLOCK_SIZE + HALF_WIDTH * 2][THREAD_BLOCK_SIZE];
     int i, i1;
     int j, j1, j2;
     int x, y;
 
     x = blockDim.x * blockIdx.x + threadIdx.x;
     y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    // int num_elements = num_rows * num_cols;
+
+    // int left_halo_index = (blockIdx.x - 1) * blockDim.x + threadIdx.x;
+    // if (threadIdx.x >= (blockDim.x - half_width))
+    //     if (left_halo_index < 0)
+    //         input_ts[(threadIdx.x - (blockDim.x - half_width))][y] = 0.0;
+    //     else
+    //         input_ts[(threadIdx.x - (blockDim.x - half_width))][y] = input[left_halo_index + threadIdx.y * num_cols];
+
+    // /* Load the center elements for the tile */
+    // if (x < num_rows)
+    //     input_ts[half_width + threadIdx.x][y] = input[x + threadIdx.y * num_cols];
+    // else 
+    //     input_ts[half_width + threadIdx.x][y] = 0.0;
+
+    // int right_halo_index = (blockIdx.x + 1) * blockDim.x + threadIdx.x;
+    // if (threadIdx.x < half_width) {
+    //     if (right_halo_index >= num_elements)
+    //         input_ts[threadIdx.x + (blockDim.x + half_width)][y] = 0.0;
+    //     else
+    //         input_ts[threadIdx.x + (blockDim.x + half_width)][y] = input[right_halo_index + threadIdx.y * num_cols];
+    // }
+
+    // __syncthreads();
 
     j1 = x - half_width;
     j2 = x + half_width;
@@ -95,6 +121,7 @@ __global__ void convolve_rows_kernel_optimized(float *result, float *input, int 
     result[y * num_cols + x] = 0.0f;
     for(i = i1, j = j1; j <= j2; j++, i++)
         result[y * num_cols + x] += kernel_c[j] * input[y * num_cols + x + i];
+        // result[y * num_cols + x] += kernel_c[j] * input_ts[x + i][y];
 
     return;
 }
@@ -126,11 +153,8 @@ __global__ void convolve_columns_kernel_optimized(float *result, float *input, i
     /* Convolve along row */
     result[y * num_cols + x] = 0.0f;
     for(i = i1, j = j1; j <= j2; j++, i++)
-        result[y * num_cols + x] += kernel_c[j] * input[y * num_cols + x + i];
+        result[y * num_cols + x] += kernel_c[y * num_cols + x + (i * num_cols)];
 
     return;
 }
-
-
-
-
+ 
